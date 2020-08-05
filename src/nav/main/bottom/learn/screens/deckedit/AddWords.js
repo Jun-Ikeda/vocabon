c8b8b72df5dc25e8117b2aec28a0777f683552a8
JunIkeda <jun.ikeda0121@gmail.com>
2020-08-05 22:26:20 +0900

    DeckEdit August 5th 2nd

--- src/nav/main/bottom/learn/screens/deckedit/AddWords.js
+++ src/nav/main/bottom/learn/screens/deckedit/AddWords.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import Header from '../../../../../../components/Header';
 import Icon from '../../../../../../components/Icon';
 import Color from '../../../../../../config/Color';
-import { TextInput } from 'react-native-gesture-handler';
+// import { TextInput } from 'react-native-gesture-handler';
 
 const style = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Color.background1,
+  },
+  headerIcon: {
+    color: Color.font2,
+    fontSize: 25,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
   },
 });
 
@@ -34,7 +47,9 @@ class AddWords extends Component {
     return (
       <View style={style.container}>
         <Header
-          renderLeft={() => <Icon.Ionicons name="ios-arrow-back" />}
+          renderLeft={() => (
+            <Icon.Ionicons name="ios-arrow-back" style={style.headerIcon} />
+          )}
           onPressLeft={() => navigation.goBack()}
           renderTitle={() => <Text>Add Words</Text>}
         />
@@ -45,9 +60,26 @@ class AddWords extends Component {
         <TextInput onChangeText={def => this.setState({ def })} value={def} />
         <TextInput onChangeText={eg => this.setState({ eg })} value={eg} />
         <TextInput onChangeText={cf => this.setState({ cf })} value={cf} />
+        {this.renderButtons()}
       </View>
     );
   }
+
+  renderButtons = () => {
+    const buttons = [
+      { title: 'Save', onPress: () => console.log('save') },
+      { title: 'Next', onPress: () => console.log('next') },
+    ];
+    return (
+      <View style={style.buttonsContainer}>
+        {buttons.map(button => (
+          <TouchableOpacity onPress={button.onPress}>
+            <Text>{button.title}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    );
+  };
 }
 
 export default AddWords;
