bd0bcac3aaf12cdcb6650a10757d334e7a0cd3f0
JunIkeda <jun.ikeda0121@gmail.com>
2020-09-04 06:25:02 +0900

    Sep 4th Ikeda

--- src/nav/main/bottom/learn/screens/deck/deckaddword/EachPage.js
+++ src/nav/main/bottom/learn/screens/deck/deckaddword/EachPage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
+import TagInput from 'react-native-tags-input';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const style = StyleSheet.create({
   inputContainer: {
@@ -8,6 +10,16 @@ const style = StyleSheet.create({
 });
 
 class EachPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tags: {
+        tag: '',
+        tagsArray: [],
+      },
+    };
+  }
+
   render() {
     const {
       page: { word, def, eg, cf },
@@ -27,15 +39,23 @@ class EachPage extends Component {
 
     return (
       <View>
-        {inputs.map(input => (
+        {/* {inputs.map(input => (
           <View style={style.inputContainer}>
             <Text>{input.title}</Text>
             <TextInput onChangeText={input.setState} value={input.value} />
           </View>
-        ))}
+        ))} */}
+        <TagInput updateState={this.updateTagState} tags={this.state.tags} />
+        <TouchableOpacity onPress={() => console.log(this.state.tags)}><Text>a</Text></TouchableOpacity>
       </View>
     );
   }
+
+  updateTagState = state => {
+    this.setState({
+      tags: state,
+    });
+  };
 }
 
 export default EachPage;
