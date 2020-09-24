9a80def76a4f3bde9e93a19bf84e8506cc78ffa6
Jun Ikeda <63273268+Jun-Ikeda@users.noreply.github.com>
2020-09-24 12:04:59 +0900

    Sep 24th Ikeda 3

---
+++ src/nav/main/bottom/learn/screens/deck/screens/property/PropertyNav.js
@@ -0,0 +1,9 @@
+import Property from './Property';
+import Title from './Title';
+import Tags from './Tags';
+
+export default {
+  property: { screen: Property },
+  title: { screen: Title },
+  tags: { screen: Tags },
+};
